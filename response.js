22c84753699d9c3e8bbe8c1b7f184a335215cbcb
Eeja07 <mahijapradipta86@gmail.com>
2025-04-09 09:04:39 +0700

    feat:add module exports and refactor code

--- response.js
+++ response.js
@@ -2,34 +2,14 @@ const response = (statusCode, data, message, res) => {
     res.status(statusCode).json({
         payload: {
             status: statusCode,
-            datas: data,
+            datas: data || null, 
         },
-        message: message,
+        message: message || "",
         pagination: {
             prev: "",
             next: "",
-            max: "",
+            current: "",
         }
-    })
-}
+    });
+};
 module.exports = response;
-// // id: 0,
-// // first_name: "",
-// // last_name: "",
-// // email: "",
-// // alt_email: "",
-// // phone: "",
-// // alt_phone: "",
-// // country_code_main: "",
-// // country_code_alt: "",
-// // gender: "",
-// // birthdate: "",
-// // home_address: "",
-// // work_address: "",
-// // profile_image: "",
-// // captured_image: "",
-// // is_profile_complete: "",
-// // is_email_verified: "",
-// // is_phone_verified: "",
-// // is_alt_email_verified: "",
-// // is_alt_phone_verified: "",
